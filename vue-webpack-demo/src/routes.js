7ff19e08d679d2c8fad93f94c67ee27cd3165122
dubiaoqi <dubiaoqi@jd.com>
2017-07-21 19:54:05 +0800

    代码提交，待跑通

---
+++ vue-webpack-demo/src/routes.js
@@ -0,0 +1,24 @@
+/**
+ * Created by dubiaoqi on 2017/7/21.
+ */
+// 引用模板
+import Vue from 'vue';
+import Router from 'vue-router';
+import indexPage from './components/header.vue';
+import homePage from './views/home.vue'
+import aboutPage from './views/about.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    routes:[
+        {
+            path:'/',
+            component:homePage
+        },
+        {
+            path:'/about',
+            component:aboutPage
+        }
+    ]
+})
