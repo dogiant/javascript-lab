926b058aebc2f0e32c5b26897d736a3e031301a8
dubiaoqi <dubiaoqi@jd.com>
2017-08-22 18:34:01 +0800

    后台界面

--- todos/src/router/index.js
+++ todos/src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Todos from '@/components/Todos'
 import VuexTest from '@/components/VuexTest'
+import Console from '@/components/Console'
 
 Vue.use(Vuex)
 Vue.use(Router)
@@ -24,6 +25,11 @@ export default new Router({
       path: '/vuex',
       name: 'VuexTest',
       component: VuexTest
+    },
+    {
+      path: '/console',
+      name: 'Console',
+      component: Console
     }
   ]
 })
